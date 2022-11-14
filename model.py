c0fb7ff1830eed953d347f868709dab419f44a68
Duje Radicic <duje.radicic.00@fesb.hr>
2022-11-14 18:43:13 +0100

    update model.py

--- model.py
+++ model.py
@@ -7,17 +7,20 @@ import pandas as pd
 
 Centre_of_Buoyancy_ix, Prismatic_Coefficient_ix, Froude_Number_ix = 0, 1, 5
 
+
 class AttributeAdder(BaseEstimator, TransformerMixin):
     def __init__(self, add_Cp_Fn_ratio=True):
         self.add_Cp_Fn_ratio = add_Cp_Fn_ratio
-    
+
     def fit(self, X, y=None):
         return self
-    
+
     def transform(self, X):
-        Lbc_Fn_ratio = X[:, Centre_of_Buoyancy_ix] / X[:, Froude_Number_ix] 
+        print(X)
+        Lbc_Fn_ratio = X[:, Centre_of_Buoyancy_ix] / X[:, Froude_Number_ix]
         if self.add_Cp_Fn_ratio:
-            Cp_Fn_ratio = X[:, Prismatic_Coefficient_ix] / X[:, Froude_Number_ix] 
+            Cp_Fn_ratio = X[:, Prismatic_Coefficient_ix] / \
+                X[:, Froude_Number_ix]
             return np.c_[X, Lbc_Fn_ratio, Cp_Fn_ratio]
         else:
             return np.c_[X, Lbc_Fn_ratio]
@@ -25,37 +28,73 @@ class AttributeAdder(BaseEstimator, TransformerMixin):
 
 # create a transformation pipeline for numerical attributes
 yacht_pipeline = Pipeline([
-            ("imputer", SimpleImputer(strategy="median")), # impute the missing values
-            ("attributes_adder", AttributeAdder()),        # add new attributes
-            ("features_scaler", StandardScaler()),         # scale the data
-        ])
+    ("imputer", SimpleImputer(strategy="median")),  # impute the missing values
+    ("attributes_adder", AttributeAdder()),         # add new attributes
+    ("features_scaler", StandardScaler()),          # scale the data
+])
 
 # column names from request
-columns = ["Length", "Beam", "Draft", "Displacement", 
-           "Centre_of_Buoyance", "Prismatic_Coefficient", "Froude_Number"]
+columns = ["length_wl", "beam_wl", "draft", "displacement",
+           "centre_of_buoyancy", "prismatic_coefficient", "velocity"]
 
 # transformed column names for the ml model
-columns_tr = ["Centre_of_Buoyancy", "Prismatic_Coefficient", "Length/Displacement_Ratio", "Beam/Draft_Ratio", 
+columns_tr = ["Centre_of_Buoyancy", "Prismatic_Coefficient", "Length/Displacement_Ratio", "Beam/Draft_Ratio",
               "Length/Beam_Ratio", "Froude_Number"]
+
 
 def predict_resistance(parameters, model):
 
-    df = pd.DataFrame(parameters, columns=columns)
+    df = pd.DataFrame(parameters, columns=columns, index=[0])
 
     df_tr = pd.DataFrame(columns=columns_tr)
 
-    df_tr["Length/Displacement_Ratio"] = df["Length"] / df["Displacement"]
+    df_tr["Length/Displacement_Ratio"] = df["length_wl"] / \
+        np.power(df["displacement"], (1/3))
 
-    df_tr["Beam/Draft_Ratio"] = df["Beam"] / df["Draft"]
-    
-    df_tr["Length/Beam_Ratio"] = df["Length"] / df["Beam"]
+    df_tr["Beam/Draft_Ratio"] = df["beam_wl"] / df["draft"]
 
-    df_tr["Centre_of_Buoyancy"] = df["Centre_of_Buoyancy"]
+    df_tr["Length/Beam_Ratio"] = df["length_wl"] / df["beam_wl"]
 
-    df_tr["Prismatic_Coefficient"] = df["Prismatic_Coefficient"]
+    df_tr["Centre_of_Buoyancy"] = df["centre_of_buoyancy"]
 
-    df_tr["Prismatic_Coefficient"] = df["Prismatic_Coefficient"]
+    df_tr["Prismatic_Coefficient"] = df["prismatic_coefficient"]
 
-    prepared_df = yacht_pipeline.transform(df_tr)
+    """
+        Velocity [m/s] = Velocity [knots] * 0.51444       
+
+                         Velocity [m/s]
+        Froude Number = ----------------
+                          sqrt( g * Lwl )
+        
+        g [m/s^2] - gravitational acceleration
+
+        Lwl [m] - waterline length
+
+    """
+
+    kt_ms = 0.51444
+
+    g = 9.81
+
+    df_tr["Froude_Number"] = (df["velocity"] * kt_ms) / \
+        np.sqrt(g * df["length_wl"])
+
+    """
+                         Resistance * 1000   
+        y_predicted = ------------------------
+                       displacement * rho * g
+
+        Resistance [N] = (y_predicted * displacement * rho * g) / 1000
+
+        rho = 1025 [kg/m^3] - seawater density
+
+    """
+
+    prepared_df = yacht_pipeline.fit_transform(df_tr)
     y_predicted = model.predict(prepared_df)
-    return y_predicted
+
+    rho = 1025
+
+    resistance = y_predicted * df["displacement"] * rho * g / 1000
+
+    return resistance.round(2).item()
