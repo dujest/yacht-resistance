7f7753e8c4b88599cb0bb5b54626fe0f3377049e
Duje Radicic <duje.radicic.00@fesb.hr>
2023-09-15 20:19:24 +0200

    switch from Flask to FastAPI

--- app.py
+++ app.py
@@ -1,22 +1,40 @@
-from flask import Flask, request, jsonify
 import pickle
 from model import predict_resistance
+from typing import Dict
+from pydantic import BaseModel
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+import uvicorn
+from mangum import Mangum
 
-app = Flask(__name__)
+
+class YachtModel(BaseModel):
+    length_wl: float
+    beam_wl: float
+    draft: float
+    displacement: float
+    centre_of_buoyancy: float
+    prismatic_coefficient: float
+    velocity: float
 
 
-@app.route('/predict', methods=['POST'])
-def predict():
+app = FastAPI()
 
-    yacht = request.get_json()
+handler = Mangum(app)
+
+@app.post('/predict')
+def predict(yacht: YachtModel):
 
     with open('./model.bin', 'rb') as f_in:
         model = pickle.load(f_in)
         f_in.close()
 
-    prediction = predict_resistance(yacht, model)
+    prediction = predict_resistance(yacht.dict(), model)
 
     result = {
         'resistance': prediction
     }
-    return jsonify(result)
+    return JSONResponse(result)
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="0.0.0.0", port=9000)
